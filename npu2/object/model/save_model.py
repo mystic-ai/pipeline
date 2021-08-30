e226ee593026012872f722f507856e2f59f25c7e
Paul Hetherington <paul@neuro-ai.co.uk>
2021-08-30 11:48:47 +0100

    updates

--- npu2/object/model/save_model.py
+++ npu2/object/model/save_model.py
@@ -1,16 +1,23 @@
-'''
 import os
 import json
 
 from npu2.object.model import Model, __model_to_dict__
 from npu2.pipeline import Pipeline, __pipline_to_dict__
 
+
 def save_model(pipeline: Pipeline, path: str):
     model_dict = __model_to_dict__(pipeline)
 
     with open("%s/model.json" % (path), "w") as conf_file:
-        conf_file.write(json.dumps({"name":pipeline.name, "pipeline_array_length":len(pipeline.pipeline_array)}))
-    
+        conf_file.write(
+            json.dumps(
+                {
+                    "name": pipeline.name,
+                    "pipeline_array_length": len(pipeline.pipeline_array),
+                }
+            )
+        )
+
     for item in range(0):
         for index, pipeline_func in enumerate(pipeline_dict["pipeline_array"]):
             if not os.path.exists("%s/" % (path)):
@@ -18,5 +25,3 @@ def save_model(pipeline: Pipeline, path: str):
 
             with open("%s/%u.nf" % (path, index), "wb") as pipline_func_file:
                 pipline_func_file.write(pipeline_func)
-
-'''
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="npu2",
+    version="0.0.1",
+    author="Paul Hetherington - Neuro",
+    description="The npu v2 client",
+    packages=["npu2"],
+)
