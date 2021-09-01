1d6e3af4dfa81ea2472126c6322269f47bdc03d4
Paul Hetherington <paul@neuro-ai.co.uk>
2021-09-01 22:02:38 +0100

    things

---
+++ npu2/api/pipeline/create_pipeline.py
@@ -0,0 +1,13 @@
+from npu2.api.call import post
+
+
+def create_pipeline(pipeline_name="undefined_pipeline", functions=[]):
+    pipeline_name = "undefined_pipeline" if pipeline_name == "" else pipeline_name
+    print(pipeline_name)
+    pipeline_dict = {
+        "name": pipeline_name,
+    }
+
+    if functions != []:
+        pipeline_dict["functions"] = functions
+    return post("/pipeline", pipeline_dict)
