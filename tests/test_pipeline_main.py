54708faa668da251e23180a67c699a322a752670
Paul Hetherington <paul@neuro-ai.co.uk>
2021-11-02 20:03:45 +0000

    updates

--- tests/test_pipeline_main.py
+++ tests/test_pipeline_main.py
@@ -1,9 +1,8 @@
-import pytest
-
-
-def basic():
-    return True
+from pipeline import Pipeline
 
 
 def test_check_running():
-    assert basic()
+
+    with Pipeline("test") as my_pipeline:
+        assert Pipeline._current_pipeline != None
+        assert Pipeline._current_pipeline.name == "test"
