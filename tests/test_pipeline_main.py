37a2d7fc8a45e56f0c0628124f25a4b6a5626241
Paul Hetherington <paul@neuro-ai.co.uk>
2021-11-02 20:16:41 +0000

    testing the tests

--- tests/test_pipeline_main.py
+++ tests/test_pipeline_main.py
@@ -1,8 +1,13 @@
 from pipeline import Pipeline
 
 
-def test_check_running():
+def test_with_decorator():
 
     with Pipeline("test") as my_pipeline:
         assert Pipeline._current_pipeline != None
+
+
+def test_with_decorator_name():
+
+    with Pipeline("test") as my_pipeline:
         assert Pipeline._current_pipeline.name == "test"
