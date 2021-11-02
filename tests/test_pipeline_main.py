bdb4105ca038cdc78376bc998c5d5d9a90afe4a9
Paul Hetherington <paul@neuro-ai.co.uk>
2021-11-02 20:28:15 +0000

    basic schema testing

--- tests/test_pipeline_main.py
+++ tests/test_pipeline_main.py
@@ -1,13 +1,20 @@
 from pipeline import Pipeline
 
 
+# Check if the decorator correctly uses __init__ and __enter__
 def test_with_decorator():
-
     with Pipeline("test") as my_pipeline:
         assert Pipeline._current_pipeline != None
 
 
+# Check naming
 def test_with_decorator_name():
-
     with Pipeline("test") as my_pipeline:
         assert Pipeline._current_pipeline.name == "test"
+
+
+# Test exit
+def test_with_exit():
+    with Pipeline("test") as my_pipeline:
+        ...
+    assert Pipeline.get_pipeline("test").name == "test"
