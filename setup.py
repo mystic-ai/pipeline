df4f3e1f2f61b7edef9579fe5f278987c7c1122c
bjornaer <maxemijo@gmail.com>
2021-12-21 16:48:44 +0100

    lint

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 # read the contents of your README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
