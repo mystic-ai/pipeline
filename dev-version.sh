1bdb173ef0f33989c19a0671a3784aa8045e6989
bjornaer <maxemijo@gmail.com>
2021-12-22 20:50:52 +0100

    remove dev version script as its non compliant with pep440

--- dev-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BRANCH=$(git branch --show-current)
-HASH=$(md5 <<<${BRANCH})
-BASE_VERSION=$(sed -n '/version/p' pyproject.toml)
-SUBSTR=${BASE_VERSION%\"}
-VERSION="${SUBSTR}+${HASH}\""
-sed -i.bak "s|.*version.*|$VERSION|" pyproject.toml
