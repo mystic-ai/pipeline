68eb9849ca042717c11ab48297b8031d70155db0
bjornaer <maxemijo@gmail.com>
2021-12-22 20:33:29 +0100

    remove slashes from branch name

--- dev-version.sh
+++ dev-version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 BRANCH=$(git branch --show-current)
+CLEAN_BRANCH=${BRANCH//\/}
 BASE_VERSION=$(sed -n '/version/p' pyproject.toml)
 SUBSTR=${BASE_VERSION%\"}
-VERSION="${SUBSTR}+${BRANCH}\""
+VERSION="${SUBSTR}+${CLEAN_BRANCH}\""
 sed -i.bak "s|.*version.*|$VERSION|" pyproject.toml
