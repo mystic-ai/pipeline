8b1a80f34017a7a6b54ffb0c50cf7ee108af7f12
bjornaer <maxemijo@gmail.com>
2021-12-29 19:17:38 +0100

    add some of the remote api stuff to different pipeline idea

--- pipeline/api/run/__init__.py
+++ pipeline/api/run/__init__.py
@@ -1,4 +1,3 @@
-import io
 import json
 from typing import Union, Any
 
--- pipeline/objects/paiplain.py
+++ pipeline/objects/paiplain.py
@@ -1,8 +1,20 @@
 import functools
-from typing import Any, List, Tuple
+import json
+import os
+from typing import Any, List, Tuple, Union
 
+from pipeline.api import authenticate
+from pipeline.api.call import post
+from pipeline.api.function import upload_function
+from pipeline.api.run import run_pipeline
 from pipeline.objects.function import Function
 from pipeline.objects.model import Model
+from pipeline.schemas.pipeline import (
+    PipelineCreate,
+    PipelineGet,
+    # PipelineVariableGet,
+)
+from pipeline.util.logging import _print
 
 _Results = List[Tuple[str, Any]]
 
@@ -12,12 +24,14 @@ class Paiplain:
     stages_results: _Results
     pipeline_context_name: str = None
     models: List[Model]
+    remote_id: Union[str, PipelineGet]
 
     def __init__(self, new_pipeline_name):
         self.pipeline_context_name = new_pipeline_name
         self.stages = []
         self.stages_results = []
         self.models = []
+        self.remote_id = None
 
     def get_results(self) -> List[Any]:
         return [res[1] for res in self.stages_results]
@@ -66,3 +80,51 @@ class Paiplain:
         self, model_class=None, *, file_or_dir: str = None, compress_tar=False
     ):
         pass
+    
+    def upload_pipeline(self) -> PipelineGet:
+        new_name = self.pipeline_context_name
+        _print("Uploading functions")
+        new_functions = [
+            upload_function(_function)
+            for _function in self.stages
+        ]
+
+        # new_variables: List[PipelineVariableGet] = []
+        # _print("Uploading variables")
+        # for _var in new_pipeline_graph.variables:
+        #     _var_type_file = upload_file(
+        #         io.BytesIO(python_object_to_hex(_var.type_class).encode()), "/"
+        #     )
+        #     _var_schema = PipelineVariableGet(
+        #         local_id=_var.local_id,
+        #         name=_var.name,
+        #         type_file=_var_type_file,
+        #         is_input=_var.is_input,
+        #         is_output=_var.is_output,
+        #     )
+        #     new_variables.append(_var_schema)
+
+        # new_graph_nodes = [
+        #     _node.to_create_schema() for _node in new_pipeline_graph.nodes
+        # ]
+        # new_outputs = [_output.local_id for _output in new_pipeline_graph.outputs]
+
+        pipeline_create_schema = PipelineCreate(
+            name=new_name,
+            # variables=new_variables,
+            functions=new_functions,
+            # graph_nodes=new_graph_nodes,
+            # outputs=new_outputs,
+        )
+        _print("Uploading pipeline graph")
+        request_result = post("/v2/pipelines", json.loads(pipeline_create_schema.json()))
+        pipe_get = PipelineGet.parse_obj(request_result)
+        self.remote_id = pipe_get
+        return pipe_get
+
+    def auth(self) -> None:
+        api_token = os.getenv("TOKEN")
+        authenticate(api_token)
+    
+    def run_remote(self):
+        return run_pipeline(self.remote_id)
