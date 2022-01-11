4e933ccbf556622ffcc2aea2377750b22a514502
bjornaer <maxemijo@gmail.com>
2022-01-11 20:35:19 +0100

    fix get source parameter

--- pipeline/objects/model.py
+++ pipeline/objects/model.py
@@ -19,6 +19,6 @@ class Model:
 
         self.name = name
         self.model = model
-        self.source = inspect.getsource(model)
+        self.source = inspect.getsource(model.__class__)
         self.hash = sha256(self.source.encode()).hexdigest()
         self.local_id = generate_id(10) if local_id is None else local_id
