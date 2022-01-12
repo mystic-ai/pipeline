038766fd057bdad545021447f804dc48d5f1a987
bjornaer <maxemijo@gmail.com>
2022-01-12 13:57:33 +0100

    messing with setup script to enable pre push hook

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
+G='\033[0;32m'
+Y='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${Y}Setting Pre-Push hooks...${NC}"
 cat > .git/hooks/pre-push <<- "EOF"
 #!/bin/bash
 pytest
 EOF
 
-poetry install
+chmod +x .git/hooks/pre-push
+echo -e "${G}Pre-Push hooks set.${NC}"
 
+echo -e "${Y}Installing deps...${NC}"
+poetry install
+echo -e "${G}Dependencies installed.${NC}"
+echo -e "${Y}Setting Pre-Commit hooks...${NC}"
 pre-commit install
 
 pre-commit autoupdate
+echo -e "${G}Pre-Commit hooks set.${NC}"
