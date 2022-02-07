48d6e321aa4568fd88dc9c879e35a623c652a2ec
paulh <pcj.hetherington@gmail.com>
2022-02-07 21:28:26 +0000

    updates

--- pipeline/objects/graph.py
+++ pipeline/objects/graph.py
@@ -116,9 +116,7 @@ class Graph:
                 hasattr(node_function, "class_instance")
                 and node_function.class_instance is not None
             ):
-                output = node_function.function.__pipeline_function__.function(
-                    node_function.class_instance, *function_inputs
-                )
+                output = node_function.function(*function_inputs)
             else:
                 output = node_function.function(*function_inputs)
 
