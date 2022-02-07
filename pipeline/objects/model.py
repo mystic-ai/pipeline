6f0c663123338ed49b37ef609def792a777cd4db
paulh <pcj.hetherington@gmail.com>
2022-02-07 21:06:31 +0000

    updates

--- pipeline/objects/model.py
+++ pipeline/objects/model.py
@@ -24,7 +24,8 @@ class Model:
         self.source = inspect.getsource(model.__class__)
         self.hash = sha256(self.source.encode()).hexdigest()
         self.local_id = generate_id(10) if local_id is None else local_id
-        setattr(self.model, "local_id", self.local_id)
+        if not hasattr(self.model, "local_id"):
+            setattr(self.model, "local_id", self.local_id)
 
     @classmethod
     def from_schema(cls, schema: ModelGet):
