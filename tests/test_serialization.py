c7e4dedec7e008421992253445c3d9a8fa466018
paulh <pcj.hetherington@gmail.com>
2022-02-08 18:57:18 +0000

    Model rebind check

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -94,6 +94,15 @@ test_pipeline_schema = PipelineGet.parse_obj(test_pipeline_dict)
 def test_model_serialization():
     reformed_graph = Graph.from_schema(test_pipeline_schema)
 
+    # Check basic graph is in correct format
     assert len(reformed_graph.models) == 1
     assert len(reformed_graph.functions) == 1
     assert len(reformed_graph.variables) == 2
+
+    # Check that the only node function has been correctly binded to the model
+    assert (
+        reformed_graph.nodes[0].function.class_instance
+        == reformed_graph.models[0].model
+    )
+
+    # TODO Add actual run check
