c060f4d0f4f93a6d300870d315f88e3a992b18b0
Alex Pearce <alex@mystic.ai>
2022-02-17 13:53:18 +0000

    Require `total` parameter when constructing Paginated.
    Towards NA-389.

--- pipeline/schemas/pagination.py
+++ pipeline/schemas/pagination.py
@@ -6,20 +6,29 @@ DataType = TypeVar("DataType")
 
 
 class PaginationDetails(BaseModel):
+    """Query parameters for requesting paginated resource lists."""
+
+    #: Index of resource items to begin paginating from
     skip: int
+    #: Maximum number of resources to return
     limit: int
 
 
 class Paginated(GenericModel, Generic[DataType]):
+    """Response for paginated resource lists."""
 
+    #: Index of resource items to begin paginating from
     skip: int
+    #: Maximum number of resources the `data` field will contain
     limit: int
+    #: Total number of resources available for pagination
     total: int
+    #: Resource data
     data: List[DataType]
 
     @classmethod
-    def of(cls, item_list: List[DataType], details: PaginationDetails):
+    def of(cls, item_list: List[DataType], details: PaginationDetails, total: int):
 
         return Paginated(
-            skip=details.skip, limit=details.limit, total=len(item_list), data=item_list
+            skip=details.skip, limit=details.limit, total=total, data=item_list
         )
