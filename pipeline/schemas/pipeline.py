00ac56128cf6bfe610331ec617c767d0acf31041
bjornaer <maxemijo@gmail.com>
2022-04-28 10:25:43 +0100

    expand pipeline get detailed schema

--- pipeline/schemas/pipeline.py
+++ pipeline/schemas/pipeline.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, Optional
 
 from pydantic import Field, root_validator
@@ -62,7 +63,10 @@ class PipelineGet(PipelineGetBrief, RunnableGet):
 
 
 class PipelineGetDetailed(PipelineGet):
-    ...
+    created_at: datetime
+    updated_at: datetime
+    last_run: int
+    public: bool
 
 
 class PipelineCreate(BaseModel):
