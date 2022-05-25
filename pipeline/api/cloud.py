12a56225e58810e30099571762c8575ae9f19263
Max Schulkin <maxemijo@gmail.com>
2022-05-25 10:45:10 +0100

    Update pipeline/api/cloud.py
    Co-authored-by: Neil Wang <39628660+isNeil@users.noreply.github.com>

--- pipeline/api/cloud.py
+++ pipeline/api/cloud.py
@@ -251,7 +251,7 @@ class PipelineCloud:
     def run_pipeline(
         self,
         pipeline_id_or_schema: Union[str, PipelineGet],
-        raw_data: Optional[Any],
+        raw_data: Optional[Any] = None,
         data_id: Optional[str] = None,
     ):
         """
