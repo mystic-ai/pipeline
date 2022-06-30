e644d1eab7355b4ebf945148b622516116b75b72
Ross Gray <ross@mystic.ai>
2022-06-30 16:44:34 +0100

    Remove unused filesystem cache

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,9 +1,5 @@
-import os
-
 from pipeline.api.cloud import PipelineCloud
 from pipeline.objects import Pipeline, Variable, pipeline_function, pipeline_model
-
-# from pipeline import logging
 
 __all__ = [
     "Pipeline",
@@ -12,10 +8,3 @@ __all__ = [
     "pipeline_function",
     "PipelineCloud",
 ]
-
-CACHE_DIR = os.getenv("PIPELINE_CACHE_DIR", "./cache")
-
-if not os.path.exists(CACHE_DIR):
-    os.makedirs(CACHE_DIR)
-elif not os.path.isdir(CACHE_DIR):
-    raise Exception("Cache dir '%s' is not a valid dir." % CACHE_DIR)
