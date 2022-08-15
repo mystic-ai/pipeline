4bf0936dd50949f700b818c83399a70c827f55fc
YvanBuggy <yvan@getneuro.ai>
2022-08-15 14:56:59 +0100

    add tests for small range of valid+invalid usernames

--- tests/schemas/test_validators.py
+++ tests/schemas/test_validators.py
@@ -1,4 +1,4 @@
-from pipeline.schemas.validators import valid_password
+from pipeline.schemas.validators import valid_password, valid_username
 
 INVALID_PASSWORDS = [
     "askjhs",  # < 8 chars
@@ -14,12 +14,34 @@ VALID_PASSWORDS = [
     "''f^tTw3!dT={6_P)wSsFu_hcR:s6;De:6`_#6A!eKw//'exp9RAv.QdsVZ]fWWfxT!$tJST:7K",
 ]
 
+INVALID_USERNAMES = [
+    "as",  # < 3 chars
+    "assj" * 7,  # > 24 chars
+    "akljhak~!#ah",  # contains non-alphanumeric character which is neither "-" or "_"
+]
+
+VALID_USERNAMES = [
+    "asakiuy",  # > 3 chars
+    "ass6" * 6,  # <= 24 chars
+    "akljhak-ah82_",  # allowed special chars
+]
+
 
 def test_invalid_password():
     for password in INVALID_PASSWORDS:
-        assert valid_password(password) is False
+        assert not valid_password(password)
 
 
 def test_valid_password():
     for password in VALID_PASSWORDS:
-        assert valid_password(password) is True
+        assert valid_password(password)
+
+
+def test_invalid_usernames():
+    for username in INVALID_USERNAMES:
+        assert not valid_username(username)
+
+
+def test_valid_usernames():
+    for username in VALID_USERNAMES:
+        assert valid_username(username)
