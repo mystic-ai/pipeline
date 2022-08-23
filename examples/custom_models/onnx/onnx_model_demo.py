c3d2384505a60c433f8a245b40c8ff05858c83a2
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-08-23 12:39:09 +0100

    comment

--- examples/custom_models/onnx/onnx_model_demo.py
+++ examples/custom_models/onnx/onnx_model_demo.py
@@ -5,5 +5,6 @@ from pipeline.util.torch_utils import tensor_to_list
 import torch
 input = tensor_to_list(torch.rand(1, 28, 28, device='cpu'))
 onnx_pipeline = onnx_model('./example.onnx')
+#run locally
 result = onnx_pipeline.run(['output'],{'input':input})
 print(result)
