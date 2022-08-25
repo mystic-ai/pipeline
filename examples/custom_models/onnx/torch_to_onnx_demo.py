40cd4d754c5c2dad5eba70f18af645fdc4fff404
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-08-25 15:11:26 +0100

    reduce example.onnx file

--- examples/custom_models/onnx/torch_to_onnx_demo.py
+++ examples/custom_models/onnx/torch_to_onnx_demo.py
@@ -9,17 +9,11 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-        )
+        self.linear = nn.Linear(28 * 28, 10)
 
     def forward(self, x):
         x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        logits = self.linear(x)
         return logits
 
 
