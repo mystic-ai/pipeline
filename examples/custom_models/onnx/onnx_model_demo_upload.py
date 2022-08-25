2fd2760dceadd68df3c535c474e304f163017ec1
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-08-25 14:39:05 +0100

    fix

--- examples/custom_models/onnx/onnx_model_demo_upload.py
+++ examples/custom_models/onnx/onnx_model_demo_upload.py
@@ -5,7 +5,7 @@ from pipeline.util.torch_utils import tensor_to_list
 
 onnx_pipeline = onnx_to_pipeline("./example.onnx")
 
-api = PipelineCloud(token="neuro_sk_aLGaopgrjCuomO6PV2BKFVNFIuR2sAM5")
+api = PipelineCloud(token="YOUR_API_KEY")
 uploaded_pipeline = api.upload_pipeline(onnx_pipeline)
 print(f"Uploaded pipeline: {uploaded_pipeline.id}")
 
