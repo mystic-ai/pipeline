c9fe0c0cd2abff81db146ad19cf37dffe6581154
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-08-25 13:45:21 +0100

    with upload

--- examples/custom_models/onnx/onnx_model_demo.py
+++ examples/custom_models/onnx/onnx_model_demo.py
@@ -1,5 +1,6 @@
 from pipeline import (
-    onnx_model
+    onnx_model,
+    PipelineCloud
 )
 from pipeline.util.torch_utils import tensor_to_list
 import torch
@@ -8,5 +9,18 @@ onnx_pipeline = onnx_model('./example.onnx')
 
 #run locally
 input = tensor_to_list(torch.rand(1, 28, 28, device='cpu'))
-result = onnx_pipeline.run(['output'],{'input':input})
-print(result)
+# result = onnx_pipeline.run(['output'],{'input':input})
+# api = PipelineCloud(token="neuro_sk_aLGaopgrjCuomO6PV2BKFVNFIuR2sAM5")
+api = PipelineCloud(url="http://10.0.0.151:5000", token="sudo1234")
+print("Now uploading")
+uploaded_pipeline = api.upload_pipeline(onnx_pipeline)
+
+run_result = api.run_pipeline(
+    uploaded_pipeline, [['output'],{'input':input}]
+)
+breakpoint()
+try:
+    result_preview = run_result["result_preview"]
+except KeyError:
+    result_preview = "unavailable"
+print("Run result:", result_preview)
