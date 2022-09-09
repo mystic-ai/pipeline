6acb9f18b048f8c1c3d2bf06feb36f54d6dae207
YvanBuggy <yvan@getneuro.ai>
2022-09-09 10:52:31 +0100

    update description

--- pipeline/schemas/metrics.py
+++ pipeline/schemas/metrics.py
@@ -68,8 +68,8 @@ class RunMetricsGet(BaseModel):
 
 class PipelineMetricsGet(RunMetricsGet):
     """Run metrics supplemented with pipeline meta data. Used in particular
-    for list/paginated metrics views where it is important to track which pipeline
-    is which"""
+    for list/paginated metrics views where it is important for the client to know which 
+    pipeline is which"""
 
     pipeline_id: str
     pipeline_name: str
