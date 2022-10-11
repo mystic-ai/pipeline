1a4dd987d4ed27ecaad08832b1ab0fe2cd7b3856
paulh <pcj.hetherington@gmail.com>
2022-10-11 10:57:54 +0100

    Working asyncio

---
+++ pipeline/api/asyncio/cloud.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+import io
+import json
+import os
+import requests
+import httpx
+import urllib.parse
+from http import HTTPStatus
+from typing import Any, Optional, Union
+
+
+from pipeline.exceptions.InvalidSchema import InvalidSchema
+from pipeline.exceptions.MissingActiveToken import MissingActiveToken
+from pipeline.schemas.compute_requirements import ComputeRequirements
+from pipeline.schemas.data import DataGet
+from pipeline.schemas.pipeline import PipelineGet
+from pipeline.schemas.file import FileGet, FileCreate
+
+from pipeline.schemas.run import RunCreate
+from pipeline.util import python_object_to_hex, generate_id
+
+
+FILE_CHUNK_SIZE = 200 * 1024 * 1024  # 200 MiB
+
+
+class PipelineCloud:
+    token: Optional[str]
+    url: Optional[str]
+
+    def __init__(
+        self, *, url: str = None, token: str = None, timeout: float = 30.0
+    ) -> None:
+        self.token = token or os.getenv("PIPELINE_API_TOKEN")
+        self.url = url or os.getenv("PIPELINE_API_URL", "https://api.pipeline.ai")
+        self.timeout = timeout
+
+    @staticmethod
+    def _get_raise_for_status(response: httpx.Response) -> None:
+        # A handler for errors that might be sent with messages from Top.
+        if not response.status_code == httpx.codes.OK:
+            content = response.json()
+            # Every exception has content of {detail, status_code[, headers]}
+            # TODO Some exceptions in Top send detail as a string and not a dict.
+            # These exceptions are now handled like normal HTTP exceptions.
+            # Need to rewrite these to all have the same format.
+            detail = content.pop("detail", "")
+            message = None
+            # In most cases detail is not a string but a dict.
+            if isinstance(detail, dict):
+                message = detail.pop("message", None)
+            elif not isinstance(detail, str):
+                detail = ""
+            detail = detail or ""
+            # If a message was delivered we want to show that. Otherwise it's a
+            # standard HTTP error and should be handled by raise_for_status()
+            if message is not None:
+                raise Exception(f"Error {response.status_code}: {message} {detail}")
+            else:
+                response.raise_for_status()
+
+    async def _post_file(self, endpoint, file, remote_path) -> FileGet:
+        if not hasattr(file, "name"):
+            file.name = generate_id(20)
+
+        headers = {
+            "Authorization": "Bearer %s" % self.token,
+        }
+        url = urllib.parse.urljoin(self.url, endpoint)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url,
+                headers=headers,
+                files={"file": (file.name, file, "application/octet-stream")},
+                timeout=self.timeout,
+            )
+        if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+            schema = FileCreate.__name__
+            raise InvalidSchema(schema=schema)
+        else:
+            response.raise_for_status()
+        return FileGet.parse_obj(response.json())
+
+    async def upload_file(self, file_or_path, remote_path) -> FileGet:
+
+        if isinstance(file_or_path, str):
+            # TODO: Change this to wrap the file object reader to convert to hex
+            # everytime anything is read instead of reading it all at once.
+
+            with open(file_or_path, "rb") as file:
+                buffer = file.read()
+            hex_buffer = buffer.hex()
+            return await self._post_file(
+                "/v2/files/", io.BytesIO(hex_buffer.encode()), remote_path
+            )
+        else:
+            return await self._post_file("/v2/files/", file_or_path, remote_path)
+
+    async def upload_data(self, file_or_path, remote_path) -> DataGet:
+        uploaded_file = await self.upload_file(file_or_path, remote_path)
+        uploaded_data = await self._post("/v2/data", uploaded_file.dict())
+        return DataGet.parse_obj(uploaded_data)
+
+    async def _post(self, endpoint, json_data):
+        headers = {
+            "Authorization": "Bearer %s" % self.token,
+            "Content-type": "application/json",
+        }
+
+        url = urllib.parse.urljoin(self.url, endpoint)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url, headers=headers, json=json_data, timeout=self.timeout
+            )
+
+        if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+            schema = json_data
+            raise InvalidSchema(schema=schema)
+        else:
+            self._get_raise_for_status(response)
+
+        return response.json()
+
+    async def run_pipeline(
+        self,
+        pipeline_id_or_schema: Union[str, PipelineGet],
+        raw_data_or_schema: Union[Any, DataGet],
+        compute_type: Optional[str] = None,
+        min_gpu_vram_mb: Optional[int] = None,
+    ):
+        """
+        Uploads Data and executes a Run of given pipeline over given data.
+
+            Parameters:
+                    pipeline_id_or_schema (Union[str, PipelineGet]):
+                        The id for the desired pipeline
+                        or the schema obtained from uploading it
+                    raw_data_or_schema (Union[Any, DataGet]):
+                        Raw data for Pipeline execution
+                        or schema for already uploaded data.
+                    compute_type (Optional[str]):
+                        Compute type requirement for this run (e.g. "cpu" or "gpu"). If
+                        not defined, it will fallback to that specified by the Pipeline
+                        itself (typically this is "gpu")
+                    min_gpu_vram_mb (Optional[int]):
+                        Minimum amount of GPU VRAM required. If not defined, it will
+                        fallback to that specified by the Pipeline (if applicable). This
+                        should only be specified for GPU workloads.
+
+            Returns:
+                    run (Any): Run object containing metadata and outputs.
+        """
+        # TODO: Add support for generic object inference. Only strs at the moment.
+        if not isinstance(raw_data_or_schema, DataGet):
+            temp_file = io.BytesIO(python_object_to_hex(raw_data_or_schema).encode())
+            uploaded_data = await self.upload_data(temp_file, "/")
+            _data_id = uploaded_data.id
+        elif isinstance(raw_data_or_schema, DataGet):
+            _data_id = raw_data_or_schema.id
+        else:
+            raise Exception("Must either pass a raw data, or DataGet schema.")
+
+        pipeline_id = None
+        if isinstance(pipeline_id_or_schema, str):
+            pipeline_id = pipeline_id_or_schema
+        elif isinstance(pipeline_id_or_schema, PipelineGet):
+            pipeline_id = pipeline_id_or_schema.id
+        else:
+            raise InvalidSchema(
+                schema=pipeline_id_or_schema,
+                message=(
+                    "Must either pass a pipeline id, or a pipeline get schema. "
+                    "Not object of type %s in arg 1." % str(pipeline_id_or_schema)
+                ),
+            )
+
+        compute_requirements = None
+        if min_gpu_vram_mb:
+            compute_requirements = ComputeRequirements(min_gpu_vram_mb=min_gpu_vram_mb)
+
+        run_create_schema = RunCreate(
+            pipeline_id=pipeline_id,
+            data_id=_data_id,
+            compute_type=compute_type,
+            compute_requirements=compute_requirements,
+        )
+        return await self._post("/v2/runs", json.loads(run_create_schema.json()))
