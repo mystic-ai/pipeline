4bb501a09d5eaaef30c5fb5bfb71045e6157f4bd
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-10-25 13:50:41 +0100

    add hugging face pipeline example

---
+++ examples/huggingface/local_run_hugging_face_pipeline.py
@@ -0,0 +1,55 @@
+from pipeline import (
+    Pipeline,
+    Variable,
+    pipeline_function,
+    pipeline_model,
+)
+import typing as t
+
+"""
+Example pipline from Hugging Face pipeline abstraction
+"""
+
+pipeline_name = "hf_roberta_text_classifier"
+
+@pipeline_model
+class model:
+    def __init__(self):
+        self.pipe = None
+
+    @pipeline_function(run_once=True, on_startup=True)
+    def load(self) -> bool:
+        from transformers import pipeline
+        self.pipe = pipeline(model="roberta-large-mnli")
+        return True
+
+    @pipeline_function
+    def predict(self, input: list) -> list:
+        return self.pipe(input)
+        
+with Pipeline(pipeline_name) as pipeline:
+    input = Variable(list, is_input=True)
+
+    pipeline.add_variables(
+        input,
+    )
+
+    model = model()
+    model.load()
+
+    output = model.predict(
+        input,
+    )
+
+    pipeline.output(output)
+
+hf_pipeline = Pipeline.get_pipeline(pipeline_name)
+
+# example run
+
+input = ["I love Halloween"]
+
+# run locally
+[output] = hf_pipeline.run(input)
+
+print(output)
