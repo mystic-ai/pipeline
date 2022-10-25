14fc5bf8e5d3aa7e538516a1c276cf74daac0527
paulh <pcj.hetherington@gmail.com>
2022-10-25 11:07:59 +0100

    Error handling in worker

--- pipeline/util/worker.py
+++ pipeline/util/worker.py
@@ -1,4 +1,4 @@
-import contextlib
+import io
 import sys
 
 import dill
@@ -6,17 +6,19 @@ import dill
 from pipeline.objects import Graph
 
 
-class DummyFile(object):
-    def write(self, x):
+class stdsurpress:
+    def __init__(self) -> None:
         pass
 
+    def __enter__(self):
+        self.save_stdout = sys.stdout
+        self.save_stderr = sys.stderr
+        sys.stdout = io.BytesIO()
+        sys.stderr = io.BytesIO()
 
-@contextlib.contextmanager
-def nostdout():
-    save_stdout = sys.stdout
-    sys.stdout = DummyFile()
-    yield
-    sys.stdout = save_stdout
+    def __exit__(self, type, value, traceback):
+        sys.stdout = self.save_stdout
+        sys.stderr = self.save_stderr
 
 
 class Worker:
@@ -31,17 +33,20 @@ class Worker:
         while True:
             command = sys.stdin.readline()
             data = sys.stdin.readline()
-            if command == "add-pipeline\n":
-                graph: Graph = dill.loads(bytearray.fromhex(data.strip()))
-                self.pipelines[graph.local_id] = graph
-                print("done\n", flush=True)
-            elif command == "run-pipeline\n":
-                with nostdout():
-                    run_info: dict = dill.loads(bytearray.fromhex(data.strip()))
-                    pipeline_id = run_info.get("pipeline_id")
-                    data = run_info.get("data")
-                    pipeline: Graph = self.pipelines[pipeline_id]
-                    result = pipeline.run(*data)
-                print(f"{str(result).strip()}\n", flush=True)
-            else:
-                print("bad-command\n", flush=True)
+            try:
+                if command == "add-pipeline\n":
+                    graph: Graph = dill.loads(bytearray.fromhex(data.strip()))
+                    self.pipelines[graph.local_id] = graph
+                    print("done\n", flush=True)
+                elif command == "run-pipeline\n":
+                    with stdsurpress():
+                        run_info: dict = dill.loads(bytearray.fromhex(data.strip()))
+                        pipeline_id = run_info.get("pipeline_id")
+                        data = run_info.get("data")
+                        pipeline: Graph = self.pipelines[pipeline_id]
+                        result = pipeline.run(*data)
+                    print(f"{str(result).strip()}\n", flush=True)
+                else:
+                    print("bad-command\n", flush=True)
+            except Exception:
+                print("failed\n", flush=True)
