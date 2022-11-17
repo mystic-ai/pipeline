5f19ec41522aa298780e74910df92b2223c208aa
paulcjh <ph@mystic.ai>
2022-11-17 21:56:28 +0000

    Updates

--- examples/docker/gpt_neo_docker.py
+++ examples/docker/gpt_neo_docker.py
@@ -3,8 +3,6 @@ from pipeline import Pipeline, Variable, pipeline_model, pipeline_function
 
 from pipeline.objects.environment import Environment
 
-from transformers import GPTNeoForCausalLM, GPT2Tokenizer
-
 @pipeline_model
 class PipelineGPTNeo:
     def __init__(self):
@@ -14,6 +12,7 @@ class PipelineGPTNeo:
     @pipeline_function
     def predict(self, input_data: str) -> str:
         if self.model == None:
+            from transformers import GPTNeoForCausalLM, GPT2Tokenizer
             self.model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
             self.tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
 
@@ -27,6 +26,7 @@ class PipelineGPTNeo:
         )
         gen_text = self.tokenizer.batch_decode(gen_tokens)[0]
         return gen_text
+
 
 
 with Pipeline("GPTNeo") as builder:
