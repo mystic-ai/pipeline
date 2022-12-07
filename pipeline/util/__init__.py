8b6482826c35e6bf0fa79eaa807676fc937c970a
Ross Gray <ross@mystic.ai>
2022-12-07 13:34:00 +0000

    Remove unused code

--- pipeline/util/__init__.py
+++ pipeline/util/__init__.py
@@ -5,8 +5,6 @@ from typing import Any, Optional
 from cloudpickle import dumps
 from dill import loads
 
-from pipeline.schemas.file import FileCreate
-
 
 def generate_id(length: int) -> str:
     return "".join((random.choice(string.ascii_letters) for i in range(length)))
@@ -27,10 +25,3 @@ def python_object_to_name(obj: Any) -> Optional[str]:
     except Exception:
         name = None
     return name
-
-
-def python_object_to_file_create(obj: Any, name: str = None):
-    if name is None:
-        name = generate_id(20)
-
-    return FileCreate(name=name, file_bytes=python_object_to_hex)
