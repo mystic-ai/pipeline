be768bfdd7465a5297a873808756a233aded701a
Neil Wang <39628660+isNeil@users.noreply.github.com>
2022-12-14 15:52:36 +0000

    fix util

--- pipeline/util/__init__.py
+++ pipeline/util/__init__.py
@@ -6,6 +6,8 @@ from typing import Any, Callable, Optional
 from cloudpickle import dumps
 from dill import loads
 
+from pipeline.schemas.file import FileCreate
+
 
 def generate_id(length: int) -> str:
     return "".join((random.choice(string.ascii_letters) for i in range(length)))
@@ -26,6 +28,13 @@ def python_object_to_name(obj: Any) -> Optional[str]:
     except Exception:
         name = None
     return name
+
+
+def python_object_to_file_create(obj: Any, name: str = None):
+    if name is None:
+        name = generate_id(20)
+
+    return FileCreate(name=name, file_bytes=python_object_to_hex)
 
 
 class CallbackBytesIO(io.BytesIO):
