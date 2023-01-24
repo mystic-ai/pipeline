d3762a14d0f5e05e73b71d1b4dee3a72ee86c745
Ross Gray <ross@mystic.ai>
2023-01-24 11:34:51 +0000

    Update base worker env

--- pipeline/objects/environment/premade.py
+++ pipeline/objects/environment/premade.py
@@ -1,60 +1,34 @@
 from pipeline.objects.environment import Environment
 
-# worker_environment = Environment(
-#     name="default-worker-environment",
-#     dependencies=[
-#         "loguru==0.5.3",
-#         "celery==5.2.3",
-#         "fastapi==0.79.1",
-#         "starlette-context==0.3.3",
-#         "starlette-exporter==0.12.0",
-#         "httpx==0.23.0",
-#         "alembic==1.7.6",
-#         "deepspeed==0.5.10",
-#         "asyncpg-listen==0.0.4",
-#         "cloudpickle==2.1.0",
-#         "seaborn==0.11.2",
-#         "numpy==1.21.0",
-#         "tensorflow==2.9.1",
-#         "tensorflow-hub==0.12.0",
-#         "Pillow==9.1.1",
-#         "opencv-python==<=4.5.5",
-#         "pydantic==1.9.1",
-#         "onnxruntime-gpu==1.13.1",
-#         "spacy==3.4.1",
-#         "accelerate==0.10.0",
-#         "transformers==4.21.2",
-#         "diffusers==0.6.0",
-#         "einops==0.4.1",
-#         "xgboost==1.6.2",
-#         "wandb==0.13.3",
-#         "setuptools==65.4.1",
-#         "scikit-learn==1.1.2",
-#         "sentence-transformers==2.2.2",
-#         "catboost==1.1",
-#         "arize==5.0.3",
-#     ],
-# )
-
-base_worker_environment = Environment(
-    name="base-worker-environment",
+default_worker_environment = Environment(
+    name="default-worker-environment",
     dependencies=[
-        "fastapi==0.79.1",
-        "starlette-context==0.3.3",
-        "starlette-exporter==0.12.0",
-        "httpx==0.23.1",
-        "asyncpg-listen==0.0.4",
-        "cloudpickle==2.2.0",
-        "seaborn==0.11.2",
+        "torch==1.13.0",
+        "torchvision==0.14.0",
+        "torchaudio==0.13.0",
+        "transformers==4.21.2",
+        "opencv-python==<=4.5.5",
+        "tensorflow==2.9.1",
+        "tensorflow-hub==0.12.0",
+        "detectron2 @ git+https://github.com/facebookresearch/detectron2.git@857d5de21a7789d1bba46694cf608b1cb2ea128a",
+        "deepspeed==0.5.10"
+        "seaborn==0.11.2"
         "numpy==1.21.0",
-        "Pillow==9.1.1",
-        "pydantic==1.9.1",
+        "Pillow==9.2.0",
+        "spacy['cuda113']==3.4.3",
+        "onnxruntime-gpu==1.12.1",
+        "sentence-transformers==2.2.2",
+        "accelerate==0.10.0",
+        "diffusers @ git+https://github.com/huggingface/diffusers.git@5755d16868ec3da7d5eb4f42db77b01fac842ea8"
+        "xgboost==1.6.2",
+        "einops==0.4.1",
+        "wandb==0.13.4",
+        "scikit-learn==1.1.2",
         "catboost==1.1",
-        # "/home/ubuntu/ross/pipeline-stack/pipeline",
-        "git+https://github.com/mystic-ai/pipeline.git@ross/envs",
-        # "dill==0.3.6",
+        "pywhisper==1.0.6",
     ],
 )
+
 
 worker_torch_environment = Environment(
     name="worker-torch-environment",
@@ -63,5 +37,4 @@ worker_torch_environment = Environment(
         "torchvision==0.14.0",
         "torchaudio==0.13.0",
     ],
-    extend_environments=[base_worker_environment],
 )
