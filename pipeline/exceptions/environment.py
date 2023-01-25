4c2a5a5971f11b2e5228ddf56bd6cae8e84edc72
Ross Gray <ross@mystic.ai>
2023-01-25 12:26:33 +0000

    Fix typo

--- pipeline/exceptions/environment.py
+++ pipeline/exceptions/environment.py
@@ -7,7 +7,7 @@ class EnvironmentInitializationError(Exception):
     def __init__(self, message: str, traceback: t.Optional[str] = None):
         self.message = message
         self.traceback = traceback
-        super().__init(self.message, self.traceback)
+        super().__init__(self.message, self.traceback)
 
     def __str__(self) -> str:
         return self.message
