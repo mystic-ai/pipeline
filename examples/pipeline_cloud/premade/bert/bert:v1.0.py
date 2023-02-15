8f686e799e7ab170b815afa373c9fdf0e97141e3
tginart <tginart@gmail.com>
2023-02-15 15:49:09 -0800

    Update bert:v1.0.py
    The models based off sentence-transformers make for better emebddings.
    
    For example, see: https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2.
    
    Is this the correct way to translate the code from the above into pipeline?

--- examples/pipeline_cloud/premade/bert/bert:v1.0.py
+++ examples/pipeline_cloud/premade/bert/bert:v1.0.py
@@ -18,38 +18,60 @@ from pipeline import (
 )
 from pipeline.util.torch_utils import tensor_to_list
 
+from transformers import AutoTokenizer, AutoModel
+import torch
+import torch.nn.functional as F
+
+
+#Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
 
 @pipeline_model
-class BertEmbedding:
+class SentenceEmbedding:
     def __init__(self):
         self.model = None
         self.tokenizer = None
 
     @pipeline_function
     def predict(self, input_data: str) -> list:
-        inputs = self.tokenizer(input_data, return_tensors="pt").to(0)
-        outputs = self.model(**inputs)
-        return tensor_to_list(outputs.last_hidden_state)
+        # Tokenize sentences
+        encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+
+        # Compute token embeddings
+        with torch.no_grad():
+            model_output = model(**encoded_input)
+
+        # Perform pooling
+        sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+
+        # Normalize embeddings
+        sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+        return tensor_to_list(sentence_embeddings)
 
     @pipeline_function(run_once=True, on_startup=True)
     def load(self) -> None:
         from transformers import AutoTokenizer, BertModel
-
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        self.model = BertModel.from_pretrained("bert-base-uncased")
+        
+        # Load model from HuggingFace Hub
+        self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        self.model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
         self.model.to(0)
 
 
 api = PipelineCloud()
 
-with Pipeline("bert") as builder:
+with Pipeline("all-MiniLM-L6-v2") as builder:
     input_str = Variable(str, is_input=True)
 
     builder.add_variables(
         input_str,
     )
 
-    model = BertEmbedding()
+    model = SentenceEmbedding()
     model.load()
     output_str = model.predict(
         input_str,
@@ -57,6 +79,6 @@ with Pipeline("bert") as builder:
 
     builder.output(output_str)
 
-output_pipeline = Pipeline.get_pipeline("bert")
+output_pipeline = Pipeline.get_pipeline("all-MiniLM-L6-v2")
 uploaded_pipeline = api.upload_pipeline(output_pipeline)
 print(uploaded_pipeline)
