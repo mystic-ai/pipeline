d28e08465622a01b2a07f2cf0b01813337274f1e
Ross Gray <ross@mystic.ai>
2023-05-10 16:55:15 +0100

    Add creation of envs

--- pipeline/v3/__init__.py
+++ pipeline/v3/__init__.py
@@ -1,8 +1,10 @@
 from pipeline.v3 import http
 from pipeline.v3.pipelines import run_pipeline, upload_pipeline
+from pipeline.v3.environments import create_environment
 
 __all__ = [
     "http",
     "upload_pipeline",
     "run_pipeline",
+    "create_environment",
 ]
