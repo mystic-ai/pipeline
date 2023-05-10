9141e3acac6b32b875107ecc5ed02d5da5c95cc9
Ross Gray <ross@mystic.ai>
2023-05-10 16:55:15 +0100

    Add creation of envs

---
+++ pipeline/v3/environments.py
@@ -0,0 +1,11 @@
+from pipeline.v3 import http
+
+
+def create_environment(name: str, python_requirements: list[str]) -> int:
+    res = http.post(
+        f"/v3/environments",
+        json_data={"name": name, "python_requirements": python_requirements},
+    )
+    res.raise_for_status()
+    env_id = res.json()["id"]
+    return env_id
