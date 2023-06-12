f12ebd4ae80a08af5df1813e6ed6f3453584c79a
paulh <paulcjh@protonmail.com>
2023-06-12 18:23:25 +0000

    format

--- pipeline/v3/environments.py
+++ pipeline/v3/environments.py
@@ -3,7 +3,7 @@ from pipeline.v3 import http
 
 def create_environment(name: str, python_requirements: list[str]) -> int:
     res = http.post(
-        f"/v3/environments",
+        "/v3/environments",
         json_data={"name": name, "python_requirements": python_requirements},
     )
     env_id = res.json()["id"]
