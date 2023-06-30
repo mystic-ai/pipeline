391088325a16ee504e2c3426e2295e730ac0e444
paulh <paulcjh@protonmail.com>
2023-06-30 09:50:03 +0000

    poll run

--- examples/v3/run_async.py
+++ examples/v3/run_async.py
@@ -1,9 +1,6 @@
 import datetime
-import sys
-import time
 
-from pipeline.v3.pipelines import get_pipeline_run, run_pipeline
-from pipeline.v3.schemas.runs import RunState
+from pipeline.v3.pipelines import poll_async_run, run_pipeline
 
 pointer = "ph/test:test"
 print(f"Running test pipeline async: {pointer}")
@@ -12,30 +9,15 @@ start_time = datetime.datetime.now()
 
 result = run_pipeline(
     pointer,
-    1,
+    5,
     async_run=True,
 )
 
-
-while not (
-    result.state
-    in [
-        RunState.completed,
-        RunState.failed,
-        RunState.rate_limited,
-        RunState.lost,
-        RunState.no_environment_installed,
-    ]
-):
-    result = get_pipeline_run(result.id)
-    sys.stdout.write(
-        f"\rWaiting for pipeline to complete... (state: {result.state.name})"
-    )
-    sys.stdout.flush()
-    time.sleep(0.5)
-
-print("")
-
+result = poll_async_run(
+    result.id,
+    timeout=None,
+    interval=0.5,
+)
 
 end_time = datetime.datetime.now()
 
