4c9827b2bff2447e65367e9c36e05b88c9571207
YvanBuggy <yvan@getneuro.ai>
2023-08-24 14:42:34 +0100

    paginated pointers

--- pipeline/console/targets/pipelines.py
+++ pipeline/console/targets/pipelines.py
@@ -6,7 +6,11 @@ from tabulate import tabulate
 from pipeline.cloud import http
 from pipeline.cloud.compute_requirements import Accelerator
 from pipeline.cloud.schemas import pipelines as pipelines_schema
-from pipeline.cloud.schemas.pagination import Paginated
+from pipeline.cloud.schemas.pagination import (
+    Paginated,
+    get_default_pagination,
+    to_page_position,
+)
 from pipeline.util.logging import _print
 
 
@@ -55,6 +59,18 @@ def get_parser(command_parser: "_SubParsersAction[ArgumentParser]") -> None:
         help="Pipeline name.",
         type=str,
     )
+    get_parser.add_argument(
+        "--skip",
+        "-s",
+        help="Number of pipelines to skip in paginated set.",
+        type=int,
+    )
+    get_parser.add_argument(
+        "--limit",
+        "-l",
+        help="Total number of pipelines to fetch in paginated set.",
+        type=int,
+    )
 
 
 def delete_parser(command_parser: "_SubParsersAction[ArgumentParser]") -> None:
@@ -78,11 +94,16 @@ def _get_pipeline(args: Namespace) -> None:
     # cluster_api = PipelineCloud(verbose=False)
 
     params = dict()
+    pagination = get_default_pagination()
     if name := getattr(args, "name", None):
         params["name"] = name
+    if skip := getattr(args, "skip", None):
+        pagination.skip = skip
+    if limit := getattr(args, "limit", None):
+        pagination.limit = limit
     paginated_raw_pipelines: Paginated[dict] = http.get(
         "/v3/pipelines",
-        params=params,
+        params=dict(**params, **pagination.dict()),
     ).json()
     pipelines = [
         [
@@ -124,6 +145,12 @@ def _get_pipeline(args: Namespace) -> None:
         for pipeline_raw in paginated_raw_pipelines["data"]
     ]
 
+    page_position = to_page_position(
+        paginated_raw_pipelines["skip"],
+        paginated_raw_pipelines["limit"],
+        paginated_raw_pipelines["total"],
+    )
+
     table = tabulate(
         pipelines,
         headers=[
@@ -136,6 +163,7 @@ def _get_pipeline(args: Namespace) -> None:
         tablefmt="psql",
     )
     print(table)
+    print(f"\nPage {page_position['current']} of {page_position['total']}\n")
 
 
 def _edit_pipeline(args: Namespace) -> None:
