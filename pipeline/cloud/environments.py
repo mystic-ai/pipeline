46ab99b26e14bc237d20400d27d8d95da67b6577
Paul CJ Hetherington <47833405+paulcjh@users.noreply.github.com>
2023-09-16 21:56:51 -0700

    Ph/environment warning (#309)
    * env checks
    
    * vb

--- pipeline/cloud/environments.py
+++ pipeline/cloud/environments.py
@@ -1,7 +1,74 @@
+import pkgutil
+import tempfile
+
+import pkg_resources
 from httpx import HTTPStatusError
+from pip_requirements_parser import RequirementsFile
 
 from pipeline.cloud import http
 from pipeline.util.logging import _print
+
+
+def _validate_requirements(requirements: list[str]) -> None:
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write("\n".join(requirements))
+        f.seek(0)
+        req_file = RequirementsFile.from_file(f.name)
+
+    for req in req_file.requirements:
+        if req.line is not None and req.line.startswith("git+"):
+            continue
+
+        if not req.is_pinned:
+            pkg_name = getattr(req, "name", "")
+
+            try:
+                version = pkg_resources.get_distribution(pkg_name).version
+            except Exception:
+                version = None
+
+            package_version_checks = [
+                "==",
+                ">=",
+                "<=",
+                ">",
+                "<",
+                "!=",
+                "~=",
+                "~",
+                "===",
+                "!==",
+            ]
+
+            if req.line is not None and any(
+                [check in req.line for check in package_version_checks]
+            ):
+                continue
+
+            raise ValueError(
+                f"Requirement {req.name} is not pinned. Please pin all requirements."
+                + (
+                    ""
+                    if version is None
+                    else f" Found version {req.name}=={version} locally."
+                )
+            )
+        if req.is_editable:
+            raise ValueError(
+                f"Requirement {req.name} is editable. Please remove all editable requirements."  # noqa
+            )
+
+        if req.is_local_path:
+            raise ValueError(
+                f"Requirement {req.name} is a local path. Please remove all local path requirements."  # noqa
+            )
+
+        # Check for if the package exists locally
+        if req.name is not None and not pkgutil.find_loader(req.name):
+            _print(
+                f"Pacakge {req.name} not found locally, it is highly recommended to install it as this will likely cause serialisation issues when uploading Pipelines.",  # noqa
+                level="WARNING",
+            )
 
 
 def create_environment(
@@ -9,6 +76,8 @@ def create_environment(
     python_requirements: list[str],
     allow_existing: bool = True,
 ) -> int:
+    _validate_requirements(python_requirements)
+
     try:
         res = http.post(
             "/v3/environments",
