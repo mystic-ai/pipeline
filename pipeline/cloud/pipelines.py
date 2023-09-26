ec74b9ee773e0d1ab225ec55a27f7afa8a5fe717
Matthew Cherry <matthew@mystic.ai>
2023-09-26 16:20:43 +0100

    Wrap run pipeline in retry

--- pipeline/cloud/pipelines.py
+++ pipeline/cloud/pipelines.py
@@ -221,70 +221,101 @@ def _data_to_run_input(data: t.Tuple) -> t.List[RunInput]:
     return input_array
 
 
+def _retry(run_get, retry, retry_states, retry_delay):
+    if run_get is None:
+        # First time, always run
+        return True
+
+    if not retry:
+        # Have a response and retry disabled
+        return False
+
+    state = run_get.state
+    if state == retry_states or state in retry_states:
+        _print(
+            f"Run state was {state.name}, "
+            f"sleeping for {retry_delay} seconds before retrying"
+        )
+        time.sleep(retry_delay)
+        return True
+
+    return False
+
+
 def run_pipeline(
     pipeline_id_or_pointer: t.Union[str, int],
     *data,
     async_run: bool = False,
     return_response: bool = False,
+    retry: bool = False,
+    retry_delay: float = 2.0,
+    retry_states: RunState | list[RunState] = RunState.rate_limited,
 ) -> t.Union[Run, httpx.Response]:
+    if isinstance(retry_states, RunState):
+        retry_states = [retry_states]
+
     run_create_schema = RunCreate(
         pipeline_id_or_pointer=pipeline_id_or_pointer,
         input_data=_data_to_run_input(data),
         async_run=async_run if not current_configuration.is_debugging() else True,
     )
 
-    res = http.post(
-        "/v3/runs",
-        json_data=run_create_schema.dict(),
-        raise_for_status=False,
-    )
+    # VALIDATE OPTIONS - cant return and have retries
 
-    if return_response and not current_configuration.is_debugging():
-        return res
+    run_get = None
+    while _retry(run_get, retry, retry_states, retry_delay):
+        res = http.post(
+            "/v3/runs",
+            json_data=run_create_schema.dict(),
+            raise_for_status=False,
+        )
 
-    if res.status_code == 500:
-        _print(
-            f"Failed run (status={res.status_code}, text={res.text}, "
-            f"headers={res.headers})",
-            level="ERROR",
-        )
-        raise Exception(f"Error: {res.status_code}, {res.text}", res.status_code)
-    elif res.status_code == 429:
-        _print(
-            f"Too many requests (status={res.status_code}, text={res.text})",
-            level="ERROR",
-        )
-        raise Exception(
-            "Too many requests, please try again later",
-            res.status_code,
-        )
-    elif res.status_code == 404:
-        _print(
-            f"Pipeline not found (status={res.status_code}, text={res.text})",
-            level="ERROR",
-        )
-        raise Exception("Pipeline not found", res.status_code)
-    elif res.status_code == 503:
-        _print(
-            f"Environment not cached (status={res.status_code}, text={res.text})",
-            level="ERROR",
-        )
-        raise Exception("Environment not cached", res.status_code)
-    elif res.status_code == 502:
-        _print(
-            "Gateway error",
-            level="ERROR",
-        )
-        raise Exception("Gateway error", res.status_code)
+        if return_response and not current_configuration.is_debugging():
+            return res
 
-    run_get = Run.parse_obj(res.json())
+        if res.status_code == 500:
+            _print(
+                f"Failed run (status={res.status_code}, text={res.text}, "
+                f"headers={res.headers})",
+                level="ERROR",
+            )
+            raise Exception(f"Error: {res.status_code}, {res.text}", res.status_code)
+        elif res.status_code == 429:
+            _print(
+                f"Too many requests (status={res.status_code}, text={res.text})",
+                level="ERROR",
+            )
+            raise Exception(
+                "Too many requests, please try again later",
+                res.status_code,
+            )
+        elif res.status_code == 404:
+            _print(
+                f"Pipeline not found (status={res.status_code}, text={res.text})",
+                level="ERROR",
+            )
+            raise Exception("Pipeline not found", res.status_code)
+        elif res.status_code == 503:
+            _print(
+                f"Environment not cached (status={res.status_code}, text={res.text})",
+                level="ERROR",
+            )
+            raise Exception("Environment not cached", res.status_code)
+        elif res.status_code == 502:
+            _print(
+                "Gateway error",
+                level="ERROR",
+            )
+            raise Exception("Gateway error", res.status_code)
 
-    def _print_logs():
-        for log in tail_run_logs(run_get.id):
-            _print_remote_log(log)
+        run_get = Run.parse_obj(res.json())
 
-    if current_configuration.is_debugging():
-        run_get = _run_logs_process(run_get.id)
+        def _print_logs():
+            for log in tail_run_logs(run_get.id):
+                _print_remote_log(log)
+
+        if current_configuration.is_debugging():
+            run_get = _run_logs_process(run_get.id, retry_states)
 
     return run_get
 
@@ -378,7 +409,7 @@ def poll_async_run(
 log_thread: Thread | None = None
 
 
-def _run_logs_process(run_id: str) -> Run:
+def _run_logs_process(run_id: str, retry_states: list[RunState]) -> Run:
     global log_thread
 
     _print(f"Trailing run logs ({run_id})")
@@ -420,7 +451,11 @@ def _run_logs_process(run_id: str) -> Run:
         if run_get.result is not None:
             _print(f"Result array: {run_get.result.result_array()}")
 
-    if RunState.is_terminal(run_get.state) and run_get.state != RunState.completed:
+    if (
+        RunState.is_terminal(run_get.state)
+        and run_get.state != RunState.completed
+        and run_get.state not in retry_states  # Don't raise an exception
+    ):
         error = getattr(run_get, "error", None)
 
         traceback = getattr(error, "traceback", None)
