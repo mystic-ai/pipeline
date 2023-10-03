fbc9ca00c520f22cb6d990f1beae89c561fe6bba
paulh <paulcjh@protonmail.com>
2023-10-03 14:49:16 +0000

    udpates

--- examples/debugging/run_pipeline.py
+++ examples/debugging/run_pipeline.py
@@ -3,24 +3,19 @@ from pipeline.configuration import current_configuration
 
 current_configuration.set_debug_mode(True)
 
-# output = run_pipeline(
-#     "meta/llama2-70B-chat:latest",
-#     [
-#         [
-#             {
-#                 "role": "system",
-#                 "content": "Reply with only a JSON, like {\"category\": \"{categories}\"}. Classify the tweet into one of the following categories: 'Social media', 'Cats', 'Technology', 'Politics', 'Raves', 'Lifestyle'.",  # noqa
-#             },
-#             {
-#                 "role": "user",
-#                 "content": "I love the new Meta Llama 2 model it's really good, yay Zuck!",  # noqa
-#             },
-#         ]
-#     ],
-#     {},
-# )
-
-run_pipeline(
-    "pipeline_71005b81de66408fbd628971cce9e82a",
-    4,
+output = run_pipeline(
+    "meta/llama2-70B-chat:latest",
+    [
+        [
+            {
+                "role": "system",
+                "content": "Reply with only a JSON, like {\"category\": \"{categories}\"}. Classify the tweet into one of the following categories: 'Social media', 'Cats', 'Technology', 'Politics', 'Raves', 'Lifestyle'.",  # noqa
+            },
+            {
+                "role": "user",
+                "content": "I love the new Meta Llama 2 model it's really good, yay Zuck!",  # noqa
+            },
+        ]
+    ],
+    {},
 )
