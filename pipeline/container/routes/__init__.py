2f7c026fb85b56b36f748a63dde1384a9aebd569
YvanBuggy <yvan@getneuro.ai>
2023-10-31 13:14:07 +0000

    add play router

--- pipeline/container/routes/__init__.py
+++ pipeline/container/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from pipeline.container.routes.play import router as play_router
 from pipeline.container.routes.v4 import router as v4_router
 
 router = APIRouter()
 router.include_router(v4_router)
+router.include_router(play_router)
