411f83d111237e4a6c21ac9709f6eac53bbae5f8
Ross Gray <ross@mystic.ai>
2023-11-21 13:56:34 +0000

    Add state to ClusterRunResult

--- pipeline/cloud/schemas/runs.py
+++ pipeline/cloud/schemas/runs.py
@@ -227,11 +227,10 @@ class ContainerRunResult(BaseModel):
     error: t.Optional[ContainerRunError]
 
     def outputs_formatted(self) -> t.List[t.Any]:
-        # return [output.value for output in self.outputs]
+        outputs = self.outputs or []
         output_array = []
-        for output in self.outputs:
+        for output in outputs:
             if output.type == RunIOType.file:
-                # output_array.append(output.value)
                 from pipeline.objects.graph import File
 
                 if output.file is not None and output.file.url is not None:
@@ -250,6 +249,8 @@ class ClusterRunResult(ContainerRunResult):
     updated_at: datetime
 
     pipeline_id: str
+
+    state = RunState
 
     class Config:
         orm_mode = True
