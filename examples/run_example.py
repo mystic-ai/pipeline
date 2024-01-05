527020a92a3cb1d4e695b2b3021f557faf6f8aa1
Matthew Cherry <matthew@mystic.ai>
2024-01-05 11:26:56 +0000

    Change run example

--- examples/run_example.py
+++ examples/run_example.py
@@ -4,14 +4,13 @@ from pipeline.cloud.pipelines import run_pipeline
 
 start_time = datetime.datetime.now()
 
-pointer = "betauser/simple:v1"
+pointer = "test:v1"
 
-result = run_pipeline(pointer, "a")
+result = run_pipeline(pointer, 1)
 
 end_time = datetime.datetime.now()
 
 total_time = (end_time - start_time).total_seconds()
 
-print(result.id)
 print(f"Total time taken: {total_time}, result: {result.outputs_formatted()}")
-print(f"Total time taken: {total_time}, result: {result.error}")
+print(f"Error: {result.error}")
