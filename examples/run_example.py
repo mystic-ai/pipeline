f019770efc38bba3a8db38cbf424af2270a32947
Matthew Cherry <matthew@mystic.ai>
2024-01-05 11:25:17 +0000

    Allow input exceptions to show in run outputs

--- examples/run_example.py
+++ examples/run_example.py
@@ -4,12 +4,14 @@ from pipeline.cloud.pipelines import run_pipeline
 
 start_time = datetime.datetime.now()
 
-pointer = "test:v1"
+pointer = "betauser/simple:v1"
 
-result = run_pipeline(pointer, 1)
+result = run_pipeline(pointer, "a")
 
 end_time = datetime.datetime.now()
 
 total_time = (end_time - start_time).total_seconds()
 
+print(result.id)
 print(f"Total time taken: {total_time}, result: {result.outputs_formatted()}")
+print(f"Total time taken: {total_time}, result: {result.error}")
