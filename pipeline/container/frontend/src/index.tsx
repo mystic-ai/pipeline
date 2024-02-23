903f9be04b7cf635a6f38d3c6137b5eb8aa9fc3e
plutopulp <yvan@mystic.ai>
2024-02-23 14:44:02 +0000

    tailwindcsss setup with webpack

--- pipeline/container/frontend/src/index.tsx
+++ pipeline/container/frontend/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Greeting } from "./components/Greeting";
 import { Farewell } from "./components/Farewell";
+import { IconLoading } from "./components/icons";
+import { Button } from "./components/buttons/button";
+import "./styles/global.css";
 
 const App = () => (
   <div>
     <Greeting name="Word" />
     <Farewell name="World" />
     <button>Hey</button>
-    <button>Hey</button>
+    <Button colorVariant="primary-animated">Hey</Button>
   </div>
 );
 
--- pipeline/container/frontend/webpack.config.js
+++ pipeline/container/frontend/webpack.config.js
@@ -21,7 +21,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
       },
     ],
   },
