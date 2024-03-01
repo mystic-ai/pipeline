5574a3328b6881f38ba3cb67e69247b5280835d0
plutopulp <yvan@mystic.ai>
2024-03-01 11:04:31 +0000

    isort + black

--- pipeline/container/routes/play.py
+++ pipeline/container/routes/play.py
@@ -5,8 +5,9 @@ from fastapi.responses import FileResponse
 router = APIRouter(prefix="/play", tags=["play"])
 
 
-
 @router.get("", response_class=FileResponse)
 async def serve_root():
-    file_path = pkg_resources.resource_filename("pipeline", "container/frontend/static/index.html")
+    file_path = pkg_resources.resource_filename(
+        "pipeline", "container/frontend/static/index.html"
+    )
     return FileResponse(file_path)
