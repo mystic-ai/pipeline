317ef373065ecfaecf39b2dc435f5f83255c6007
Ross Gray <ross@mystic.ai>
2024-03-13 17:22:11 +0000

    Add async version of helper func

--- pipeline/util/streaming.py
+++ pipeline/util/streaming.py
@@ -30,3 +30,31 @@ def handle_stream_response(
                 break
 
             yield result
+
+
+async def handle_async_stream_response(
+    response: Response, result_schema_cls: T
+) -> t.AsyncGenerator[T, t.Any]:
+    """Helper function to help with parsing streamed data from an API into
+    instances of 'result_schema_cls'
+
+    Same as above but for async httpx client.
+    """
+    incomplete_chunk = ""
+    async for chunk in response.aiter_text():
+        # chunk of data coming back from API could either be an incomplete
+        # result or more than one results, so try to handle both these cases
+        full_chunk = incomplete_chunk + chunk
+        results = full_chunk.split("\n")
+        for result in results:
+            if not result:
+                continue
+            try:
+                result = result_schema_cls.parse_raw(result)
+                incomplete_chunk = ""
+            except ValidationError:
+                # assume due to an incomplete chunk of JSON
+                incomplete_chunk = result
+                break
+
+            yield result
