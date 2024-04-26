0400d86f0ff4cf0c354dab9af3394d81f4ecc8fc
plutopulp <yvan@mystic.ai>
2024-04-26 14:29:48 +0000

    change cluster schmea on pipeline get

--- pipeline/cloud/schemas/cluster.py
+++ pipeline/cloud/schemas/cluster.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class PipelineClusterConfig(BaseModel):
     id: str
     node_pool: str
+
+
+class PipelineClusterLean(BaseModel):
+    id: str
+    name: str
+    node_pool: str | None = None
+    provider: str | None = None
